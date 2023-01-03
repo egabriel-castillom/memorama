17a1b0d48a924bc0f8eba5d16aca367b0ae5cd6e
egabriel-castillom <62447576+egabriel-castillom@users.noreply.github.com>
2023-01-02 18:43:03 -0600

    Ins

--- modularmain.py
+++ modularmain.py
@@ -5,48 +5,50 @@ import os
 
 load_dotenv()
 p=0
-def assignid():
-    if session.get('id'):
-        id1 = session.get('id')
-        id2 = request.form.get('id')
-        if id1 == id2:
-            session.clear()
-            message = ' Se ha quitado la selección'
-            return message
-    else:
-        id = str(request.form.get('id'))
-        session['id'] = id                                
-        message = id + ' id en sesion.'    
-        return message    
-
-def assigniD():
-    if session.get('iD'):
-        iD1 = session.get('iD')
-        iD2 = session.get('id')
-        if iD1 == iD2:
-            session.clear()
-            message = 'Se ha quitado la selección'
-            return message
-    else:
-        session['iD'] = request.form.get('id')                   
-        message = iD1 + ' , ' + iD2 + ' ids en sesion.'
-        return message
-
-def evaluate():
-    a = float(id1)
-    b = float(iD)
-    RF = a + b
-    rf = str(RF)
-    results=[2.1, 4.1, 6.1, 8.1, 10.1, 12.1]
-    if RF in results:
-        rfo = 'CORRECTO ' + rf
-        P = 'perr' + str(p)
-        return render_template('main_memo.html',id=id1,iD=iD,message=message,rf=rfo, p=P)
-    else:
-        rfo = 'INCORRECTO ' + rf
-        return render_template('main_memo.html',id=id1,iD=iD,message=message,rf=rfo)
 
 def create_app(): #SE EJECUTA SIEMPRE QUE SE GENERE UNA INSTANCIA DE LA APLICACIÓN dentro del bash. 
+    
+     def assignid():
+        if session.get('id'):
+            id1 = session.get('id')
+            id2 = request.form.get('id')
+            if id1 == id2:
+                session.clear()
+                message = ' Se ha quitado la selección'
+                return message
+        else:
+            id = str(request.form.get('id'))
+            session['id'] = id                                
+            message = id + ' id en sesion.'    
+            return message    
+
+    def assigniD():
+        if session.get('iD'):
+            iD1 = session.get('iD')
+            iD2 = session.get('id')
+            if iD1 == iD2:
+                session.clear()
+                message = 'Se ha quitado la selección'
+                return message
+        else:
+            session['iD'] = request.form.get('id')                   
+            message = iD1 + ' , ' + iD2 + ' ids en sesion.'
+            return message
+
+    def evaluate():
+        a = float(id1)
+        b = float(iD)
+        RF = a + b
+        rf = str(RF)
+        results=[2.1, 4.1, 6.1, 8.1, 10.1, 12.1]
+        if RF in results:
+            rfo = 'CORRECTO ' + rf
+            P = 'perr' + str(p)
+            return render_template('main_memo.html',id=id1,iD=iD,message=message,rf=rfo, p=P)
+        else:
+            rfo = 'INCORRECTO ' + rf
+            return render_template('main_memo.html',id=id1,iD=iD,message=message,rf=rfo)
+
     app = Flask(__name__) #Asigna modulo inicial para nuestra aplicación.
    
     app.config.from_mapping( #Configuración de nuestra app, obtiene las variables de entorno necesarias para ejecutar conexion con db.  
@@ -56,12 +58,12 @@ def create_app(): #SE EJECUTA SIEMPRE QUE SE GENERE UNA INSTANCIA DE LA APLICACI
     def memo():
         if request.method == 'POST':
             message = 'posteando heh.'                                                 
-            #if session.get('id'):
-            #    message = assigniD()
-            #    return render_template('main_memo.html',message=message)
-            #else:
-            #    message = assignid()
-            #    return render_template('main_memo.html',message=message)
+            if session.get('id'):
+                message = assigniD()
+                return render_template('main_memo.html',message=message)
+            else:
+                message = assignid()
+                return render_template('main_memo.html',message=message)
             return render_template('main_memo.html', message=message)
         return render_template('main_memo.html')
     return app
