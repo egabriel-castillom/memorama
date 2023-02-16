11c321fdd5501457cb66066135744bea0212b9c7
egabriel-castillom <62447576+egabriel-castillom@users.noreply.github.com>
2023-02-16 11:34:44 -0600

    check if works

--- main.py
+++ main.py
@@ -14,54 +14,8 @@ def create_app(): #SE EJECUTA SIEMPRE QUE SE GENERE UNA INSTANCIA DE LA APLICACI
     def memo():
         if request.method == 'POST':                                                             
             if session.get('id') and session.get('iD'):
-#                if session.get('id1'):
-#                    id1 = session.get('id1')
-#                    id2 = request.form.get('id')
-#                    if id1 == id2:
-#                        session.clear()
-#                        message = ' Se ha quitado la selección'
-#                        return render_template('main_memo.html',message=message)                                     
-#                    else:
-#                        if session.get('iD1'):
-#                            iD1 = session.get('iD1')
-#                            iD2 = id2
-#                            if iD1 == iD2:
-#                                session.clear()
-#                                message = ' Se ha quitado la selección'
-#                                return render_template('main_memo.html',message=message)
-#                        else:
-#                            session['iD1'] = id2                    
-#                            message = id1 + ' , ' + id2 + ' ids en sesion.'             
-#                            a = float(id1)
-#                            b = float(id2)
-#                            RF = a + b
-#                            rf = str(RF)
-#                            results=[2.1, 4.1, 6.1, 8.1, 10.1, 12.1]
-#                            if RF in results:
-#                                rfo = 'CORRECTO ' + rf
-#                                P1 = str(P+1)
-#                                return render_template('main_memo.html',id=id1,iD=id2,message=message,rf=rfo, p=P)
-#                            else:
-#                                rfo = 'INCORRECTO ' + rf
-#                                return render_template('main_memo.html',id=id1,iD=id2,message=message,rf=rfo)                       		       
-                    message = 'Si jala'
-                    return render_template('main_memo.html',id=id1,iD=id2,message=message)
-                else:
-                    id1 = request.form.get('id')
-                    id = session.get('id')
-                    iD = session.get('iD')
-                    if id1 == id:
-                        message = 'ID ' + id1 + ' actualmente en sesion.'
-                        return render_template('main_memo.html',id=id1,iD=iD,p=P, message=message)
-                    elif id1 == iD:
-                        message = 'ID ' + id1 + ' actualmente en sesion.'
-                        return render_template('main_memo.html',id=id,iD=id1,p=P, message=message)
-                    else:
-                        session['id1'] = id1                                
-                        message = id1 + ' id en sesion.'
-                        return render_template('main_memo.html', id=id1,iD=iD,id1=id1,message=message, p=P)        
-                        #session.clear()
-                        #click.echo('SESION REINICIADA.')
+                message = 'Si jala'
+                return render_template('main_memo.html',id=id1,iD=id2,message=message)
             else:    
                 if session.get('id'):
                     id1 = session.get('id')
