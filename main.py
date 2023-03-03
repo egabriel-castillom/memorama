c609fb46b035c312a0257cf7e9b6d994e22d2b91
egabriel-castillom <62447576+egabriel-castillom@users.noreply.github.com>
2023-03-03 10:37:51 -0600

    NIR

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def create_app(): #SE EJECUTA SIEMPRE QUE SE GENERE UNA INSTANCIA DE LA APLICACI
                             session['P'] = r2P
                             return render_template('main_memo.html',id=id,iD=iD,id1=r2id,iD1=r2iD,message=message, p=r2P, rf = r2rfo)
                         else:
-                            r2rfo = 'INCORRECTO ' + r2rf + ' Vuelve a intentarlo.'
+                            r2rfo = 'NO SE ENCUENTRA EN RESULTADOS | id ' + r2id + ' + iD ' + r2iD + ' = ' + r2rf
                             session['F'] = 1
                             F = session.get('F')
                             return render_template('main_memo.html',id=id,iD=iD,rf=r2rfo,F=F)                       		        	                            
@@ -74,17 +74,17 @@ def create_app(): #SE EJECUTA SIEMPRE QUE SE GENERE UNA INSTANCIA DE LA APLICACI
                         return render_template('main_memo.html',id=id,iD=iD,id1=r2id,message=message, p=P)
             else:    
                 if session.get('id'):
-                    id1 = session.get('id')
-                    id2 = request.form.get('id')
-                    if id1 == id2:
+                    id = session.get('id')
+                    iD = request.form.get('id')
+                    if id == iD:
                         session.clear()
                         message = ' Se ha quitado la selección'
                         return render_template('main_memo.html',message=message)                                     
                     else:
-                        session['iD'] = id2                    
-                        message = id1 + ' , ' + id2 + ' ids en sesion.'             
-                        a = float(id1)
-                        b = float(id2)
+                        session['iD'] = iD                    
+                        message = id + ' , ' + iD + ' ids en sesion.'             
+                        a = float(id)
+                        b = float(iD)
                         RF = a + b
                         rf = str(RF)
                         results=[2.1, 4.1, 6.1, 8.1, 10.1, 12.1]
@@ -92,12 +92,12 @@ def create_app(): #SE EJECUTA SIEMPRE QUE SE GENERE UNA INSTANCIA DE LA APLICACI
                             rfo = 'CORRECTO ' + rf
                             P = str(p+1)
                             session['P'] = P
-                            return render_template('main_memo.html',id=id1,iD=id2,message=message,rf=rfo, p=P)
+                            return render_template('main_memo.html',id=id,iD=iD,message=message,rf=rfo, p=P)
                         else:
-                            rfo = 'INCORRECTO ' + rf + ' Vuelve a intentarlo.'
+                            rfo = 'NO SE ENCUENTRA EN RESULTADOS | id ' + id + ' + iD ' + iD + ' = ' + rf
                             session['F'] = 1
                             F = session.get('F')
-                            return render_template('main_memo.html',id=id1,iD=id2,rf=rfo,F=F)                       		        	
+                            return render_template('main_memo.html',id=id,iD=iD,rf=rfo,F=F)                       		        	
                 else:
                     id = str(request.form.get('id'))
                     session['id'] = id                                
