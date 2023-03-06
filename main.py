dcdb9f39f74a2d3454fa3d07b5c0d69daaf8ae02
egabriel-castillom <egabriel.castillom@gmail.com>
2023-03-06 17:47:43 -0600

    mainupdate

--- main.py
+++ main.py
@@ -28,7 +28,15 @@ def create_app(): #SE EJECUTA SIEMPRE QUE SE GENERE UNA INSTANCIA DE LA APLICACI
                     session.clear()
                     return render_template('main_memo.html')
             elif session.get('P') == '2':
-                message = 'TERCERA RONDA WE'
+                r3id = request.form.get('id')
+                id = session.get('id')
+                iD = session.get('iD')
+                r2id = session.get('r2id')
+                r2iD = session.get('r2iD')
+                PA = 'PUNTAJE ACTUAL: ' + session.get('P')
+                if r3id == id:
+                    message = 'Ya se ha seleccionado ' + id + ' en la primer sesión.'
+                    return render_template('main_memo.html',message=message,id=id,iD=iD,id1=r2id,iD1=r2iD, p=PA)
                 return render_template('main_memo.html',message=message)
             elif session.get('P'):
                 id = session.get('id')
