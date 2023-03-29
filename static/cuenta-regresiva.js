2af212990766c1fffb3f90c81500493949654818
egabriel-castillom <egabriel.castillom@gmail.com>
2023-03-29 17:36:50 -0600

    jsUPDT

--- static/cuenta-regresiva.js
+++ static/cuenta-regresiva.js
@@ -1,9 +1,10 @@
-let tiempoRestante = 60; // 60 segundos
+let tiempoRestante = localStorage.getItem('tiempoRestante') || 60; // 60 segundos
 const elementoCuentaRegresiva = document.getElementById('cuenta-regresiva');
 
 const actualizarCuentaRegresiva = () => {
   elementoCuentaRegresiva.textContent = `${tiempoRestante} segundos restantes`;
   tiempoRestante -= 1;
+  localStorage.setItem('tiempoRestante', tiempoRestante);
   if (tiempoRestante >= 0) {
     setTimeout(actualizarCuentaRegresiva, 1000);
   }
