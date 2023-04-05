5a472ad2bb7b77e3bd7607fb9a8a8f8270377558
egabriel-castillom <egabriel.castillom@gmail.com>
2023-04-05 15:59:25 -0600

    indicesUPDT

---
+++ static/indices.ts
@@ -0,0 +1,6 @@
+if (sessionStorage.getItem('indices')) {
+    let elements: NodeListOf<Element> = document.querySelectorAll('.marco');
+    let parent: Node = elements[0].parentNode;
+    let indices: number[] = JSON.parse(sessionStorage.getItem('indices'));
+    indices.forEach(i => parent.appendChild(elements[i]));
+}
