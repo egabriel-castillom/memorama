dfbd1e9e8f5bfae5baaa652382f8140f0b362eec
egabriel-castillom <egabriel.castillom@gmail.com>
2023-04-05 17:33:04 -0600

    lastONE

--- static/ran.js
+++ static/ran.js
@@ -7,22 +7,24 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     }
     return to.concat(ar || Array.prototype.slice.call(from));
 };
-var _a;
-// Obtener todos los elementos que deseas reorganizar
-var elements = document.querySelectorAll('.carta');
-// Crear una lista de índices
-var indices = __spreadArray([], Array(elements.length), true).map(function (_, i) { return i; });
-// Mezclar aleatoriamente la lista de índices
-for (var i = indices.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    _a = [indices[j], indices[i]], indices[i] = _a[0], indices[j] = _a[1];
-}
-// Reorganizar los elementos en el orden especificado por la lista mezclada
-var parentElement = elements[0].parentNode;
-indices.forEach(function (i) {
-    if (parentElement) {
-        parentElement.appendChild(elements[i]);
+document.addEventListener('DOMContentLoaded', function () {
+    var _a;
+    // Obtener todos los elementos que deseas reorganizar
+    var elements = document.querySelectorAll('.carta');
+    // Crear una lista de índices
+    var indices = __spreadArray([], Array(elements.length), true).map(function (_, i) { return i; });
+    // Mezclar aleatoriamente la lista de índices
+    for (var i = indices.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        _a = [indices[j], indices[i]], indices[i] = _a[0], indices[j] = _a[1];
     }
+    // Reorganizar los elementos en el orden especificado por la lista mezclada
+    var parentElement = elements[0].parentNode;
+    indices.forEach(function (i) {
+        if (parentElement) {
+            parentElement.appendChild(elements[i]);
+        }
+    });
+    // Almacenar el orden de los elementos en una variable de sesión
+    sessionStorage.setItem('indices', JSON.stringify(indices));
 });
-// Almacenar el orden de los elementos en una variable de sesión
-sessionStorage.setItem('indices', JSON.stringify(indices));
