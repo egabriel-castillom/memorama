a6dcc9c3c51e17243cd9dbc20486971302ba3e03
egabriel-castillom <egabriel.castillom@gmail.com>
2023-04-08 14:32:28 -0600

    indicesELUPDT

--- static/indices.ts
+++ static/indices.ts
@@ -1,6 +1,8 @@
-if (sessionStorage.getItem('indices')) {
-    let elements: NodeListOf<Element> = document.querySelectorAll('.marco');
-    let parent: Node = elements[0].parentNode;
-    let indices: number[] = JSON.parse(sessionStorage.getItem('indices'));
-    indices.forEach(i => parent.appendChild(elements[i]));
-}
+document.addEventListener('DOMContentLoaded', () => {
+    if (sessionStorage.getItem('indices')) {
+        let elements: NodeListOf<Element> = document.querySelectorAll('.marco');
+        let parent: Node = elements[0].parentNode;
+        let indices: number[] = JSON.parse(sessionStorage.getItem('indices'));
+        indices.forEach(i => parent.appendChild(elements[i]));
+    }
+});
